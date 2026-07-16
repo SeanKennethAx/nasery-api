5fd67e2be7dc6241a7052b41a4927f7b2a01a337
Sean Kenneth Axalan <seankennethaxalan@gmail.com>
2026-07-16 19:49:16 +0800

    Users authentication

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,7 +25,9 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'firstname' => fake()->firstName(),
+            'middlename' => fake()->firstName(),
+            'lastname' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
---
+++ routes/unauthenticated.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Models\User;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+
+
+Route::post('/auth/login', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return $user->createToken($request->email)->plainTextToken;
+});
